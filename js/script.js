54b841cd843e5bf1b6e2e82b62b658fec31979cf
Fabiano Luiz Santos <47800473+FabianoLXS@users.noreply.github.com>
2023-04-27 21:41:55 -0300

    commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const playBtn = document.querySelector("#play")
+const pauseBtn = document.querySelector("#pause")
+const setBtn = document.querySelector("#set")
+const stopBtn = document.querySelector("#stop")
+
