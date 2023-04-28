d87ee25b51078c5d072d93e7ba0cc0ddfdf0e975
Fabiano Luiz Santos <47800473+FabianoLXS@users.noreply.github.com>
2023-04-27 22:02:06 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -3,3 +3,17 @@ const pauseBtn = document.querySelector("#pause")
 const setBtn = document.querySelector("#set")
 const stopBtn = document.querySelector("#stop")
 
+playBtn.addEventListener
+
+
+
+//Modal
+const modal = {
+  controls: document.querySelector("#controls"),
+start() {
+  modal.controls.classList.add("hide")
+},
+stop() {
+  modal.controls.classList.remove("hide")
+},
+}
