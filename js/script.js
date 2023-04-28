3663fbea98b3bbd73c6800276f615a0d986dfad2
Fabiano Luiz Santos <fab_luiz@msn.com>
2023-04-28 17:48:33 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,20 +1,38 @@
-const playBtn = document.querySelector("#play")
-const pauseBtn = document.querySelector("#pause")
-const setBtn = document.querySelector("#set")
-const stopBtn = document.querySelector("#stop")
+const playBtn = document.querySelector(".play")
+const pauseBtn = document.querySelector(".pause")
+const setBtn = document.querySelector(".set")
+const stopBtn = document.querySelector(".stop")
+const soundOnBtn = document.querySelector(".soundOn")
+const soundOffBtn = document.querySelector(".soundOff")
 
-playBtn.addEventListener
+playBtn.addEventListener('click', function() {
+  playBtn.classList.add('hide')
+  pauseBtn.classList.remove('hide')
+})
 
-//Modal
-const modal = {
-  playBtn: document.querySelector("#play"),
-  pauseBtn: document.querySelector("#pause"),
-  setBtn: document.querySelector("#set"),
-  stopBtn: document.querySelector("#stop"),
-  start() {
-    modal.playBtn.classList.add("hide")
-  },
-  stop() {
-    modal.controls.classList.remove("hide")
-  },
-}
+pauseBtn.addEventListener("click", function () {
+  playBtn.classList.remove("hide")
+  pauseBtn.classList.add("hide")
+})
+
+setBtn.addEventListener("click", function () {
+  setBtn.classList.add("hide")
+  stopBtn.classList.remove("hide")
+})
+
+stopBtn.addEventListener("click", function () {
+  setBtn.classList.remove("hide")
+  stopBtn.classList.add("hide")
+  playBtn.classList.remove("hide")
+  pauseBtn.classList.add("hide")
+})
+
+soundOnBtn.addEventListener('click', function() {
+  soundOnBtn.classList.add("hide")
+  soundOffBtn.classList.remove("hide")
+})
+
+soundOffBtn.addEventListener("click", function () {
+  soundOnBtn.classList.remove("hide")
+  soundOffBtn.classList.add("hide")
+})
