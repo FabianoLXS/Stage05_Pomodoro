4972670368499f7b491603e0675aa7fb065f26b6
Fabiano Luiz Santos <fab_luiz@msn.com>
2023-04-29 19:28:36 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -12,18 +12,26 @@ function countDown() {
   setTimeout(function () {
     let seconds = Number(secondsDisplay.textContent)
     let minutes = Number(minutesDisplay.textContent)
+    
+    secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
+    
+    if (minutes <= 0) {
+      playBtn.classList.remove('hide')
+      pauseBtn.classList.add('hide')
+      setBtn.classList.remove('hide')
+      stopBtn.classList.add('hide')
+      
+      return
+    }
+    
     if(seconds <= 0) {
       seconds = 60
       
       minutesDisplay.textContent = String(minutes - 1).padStart(2, "0")
     }
-    
-    secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
-
-    if (minutes <= 0) {
-      return
-    }
-
+   
+   secondsDisplay.textContent = String(seconds - 1).padStart(2, "0") 
+  
    countDown()
   }, 1000)
 }
