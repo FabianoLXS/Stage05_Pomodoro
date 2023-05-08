290e265446187a07d1451915a3ecb42acd601ce5
Fabiano Luiz Santos <47800473+FabianoLXS@users.noreply.github.com>
2023-05-08 17:52:42 -0300

    commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+import resetControls from "./controls.js"
+import "./timer.js"
+
 const playBtn = document.querySelector(".play")
 const pauseBtn = document.querySelector(".pause")
 const setBtn = document.querySelector(".set")
@@ -8,52 +11,6 @@ const minutesDisplay = document.querySelector("#minutes")
 const secondsDisplay = document.querySelector("#seconds")
 let minutes
 let timerTimeOut
-
-
-function resetControls() {
-  buttonPlay.classList.remove("hide")
-  buttonPause.classList.add("hide")
-  buttonSet.classList.remove("hide")
-  buttonStop.classList.add("hide")
-}
-
-function resetTimer() {
-updateTimerDisplay(minutes, 0)
-clearTimeout(timerTimeOut)
-}
-
-function updateTimerDisplay(minutes, seconds) {
-  minutesDisplay.textContent = String(minutes - 1).padStart(2, "0")
-  secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
-}
-
-function countDown() {
-  timerTimeOut = setTimeout(function () {
-    let seconds = Number(secondsDisplay.textContent)
-    let minutes = Number(minutesDisplay.textContent)
-    
-    secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
-    
-    if (minutes <= 0) {
-      playBtn.classList.remove('hide')
-      pauseBtn.classList.add('hide')
-      setBtn.classList.remove('hide')
-      stopBtn.classList.add('hide')
-      
-      return
-    }
-    
-    if(seconds <= 0) {
-      seconds = 60
-      
-      minutesDisplay.textContent = String(minutes - 1).padStart(2, "0")
-    }
-   
-   secondsDisplay.textContent = String(seconds - 1).padStart(2, "0") 
-  
-   countDown()
-  }, 1000)
-}
 
 playBtn.addEventListener('click', function() {
   playBtn.classList.add('hide')
---
+++ js/timer.js
@@ -0,0 +1,37 @@
+function resetTimer() {
+  updateTimerDisplay(minutes, 0)
+  clearTimeout(timerTimeOut)
+}
+
+function updateTimerDisplay(minutes, seconds) {
+  minutesDisplay.textContent = String(minutes - 1).padStart(2, "0")
+  secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
+}
+
+function countDown() {
+  timerTimeOut = setTimeout(function () {
+    let seconds = Number(secondsDisplay.textContent)
+    let minutes = Number(minutesDisplay.textContent)
+
+    secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
+
+    if (minutes <= 0) {
+      playBtn.classList.remove("hide")
+      pauseBtn.classList.add("hide")
+      setBtn.classList.remove("hide")
+      stopBtn.classList.add("hide")
+
+      return
+    }
+
+    if (seconds <= 0) {
+      seconds = 60
+
+      minutesDisplay.textContent = String(minutes - 1).padStart(2, "0")
+    }
+
+    secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
+
+    countDown()
+  }, 1000)
+}
